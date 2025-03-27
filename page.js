a950b2e7e82376e28dcf4368881111aab2009a5a
MANOJ TARAD <142465528+ManojTarad65@users.noreply.github.com>
2025-03-27 18:33:24 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../../component/header'
+import Footer from '../../component/Footer'
+import Main from '../../component/Main'
+
+const page = () => {
+  return (
+    <div>
+
+
+  
+   
+      <Main />
+
+     
+    </div>
+  )
+}
+
+export default page
